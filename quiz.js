36a4907fe8b47ecc7acc4f660b79cc13935b64db
Abel-G <abeljeb@gmail.com>
2024-07-14 09:16:05 +0300

    adding strict comparision

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer(){
     let correctAnswer = "4";
     const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
     const p = document.querySelector('#feedback');
-    if (userAnswer == correctAnswer){
+    if (userAnswer === correctAnswer){
         p.textContent = "Coreect! well done."
     } else {
         p.textContent = "That's incorrect. Try again!";
