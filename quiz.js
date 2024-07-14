fe5ca161f7c3567bbbbc76f7947695e9dd3a9f01
Abel-G <abeljeb@gmail.com>
2024-07-14 09:13:33 +0300

    final answer

--- quiz.js
+++ quiz.js
@@ -1,3 +1,17 @@
 function checkAnswer(){
     let correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    const p = document.querySelector('#feedback');
+    if (userAnswer == correctAnswer){
+        p.textContent = "Coreect! well done."
+    } else {
+        p.textContent = "That's incorrect. Try again!";
+    }
 }
+
+const btn = document.getElementById('submit-answer');
+
+btn.addEventListener('click', function(e) {
+    checkAnswer();
+})
+
